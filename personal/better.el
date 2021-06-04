dfd1a6675028eff4846da5609d37e35e2b61f526
akshitkr <akshitkumar31102003@gmail.com>
2021-06-04 17:20:23 +0530

    misc

--- personal/better.el
+++ personal/better.el
@@ -1,3 +1,5 @@
+;;; miscellaneous stuff
+
 ;;; esc always quits
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
@@ -5,3 +7,15 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 (global-set-key [escape] 'keyboard-escape-quit)
+
+;;; remove whitespace mode
+(setq prelude-whitespace nil)
+
+
+;;; make company mode faster
+(setq company-dabbrev-downcase 0)
+(setq company-idle-delay 0.2)
+
+;;; projectile setup
+(setq projectile-auto-discover t)
+(setq projectile-project-search-path '("~/Documents/code/python/" "~/Documents/code/c/" "~/Documents/code/node/" "~/Documents/code/react/"))
---
+++ personal/preload/keybinds.el
@@ -0,0 +1,3 @@
+
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
